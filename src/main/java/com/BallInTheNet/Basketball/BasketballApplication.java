3bc33f97c40a186d2cdbf283ec7c1d0caccb368f
Aneta <aw22@onet.pl>
2019-04-10 17:17:09 +0200

    packages and git ingore added

---
+++ src/main/java/com/BallInTheNet/Basketball/BasketballApplication.java
@@ -0,0 +1,13 @@
+package com.BallInTheNet.Basketball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasketballApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BallInTheNet/Basketball/BasketballApplicationTests.java
@@ -0,0 +1,16 @@
+package com.BallInTheNet.Basketball;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BasketballApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
